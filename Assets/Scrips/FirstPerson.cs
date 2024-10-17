4f14379356abcae00d278875163f81463b22440a
adridas16 <adridas666@gmail.com>
2024-10-17 21:15:38 +0200

    Initial commit

---
+++ Assets/Scrips/FirstPerson.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    CharacterController Controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); //h=0, h=-1,h=1
+        float v = Input.GetAxisRaw("Vertical");
+        Vector2 input = new Vector2(h, v).normalized;
+
+
+
+        if (input.magnitude > 0)
+        {
+
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg+Camera.main.transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+            Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
+            Controller.Move(movimiento* velocidadMovimiento*Time.deltaTime);
+            
+
+        }
+    }
+}
