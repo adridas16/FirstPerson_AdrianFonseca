8a040a5290db9ff3012197a5609462097e09ec13
adridas16 <adridas666@gmail.com>
2024-11-25 18:24:05 +0100

    water

--- Assets/Scrips/WaterEfect.cs
+++ Assets/Scrips/WaterEfect.cs
@@ -7,6 +7,7 @@ using UnityEngine.Rendering.Universal;
 public class WaterEfect : MonoBehaviour
 {
     private Volume efecto;
+    [SerializeField] private float velocidad;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,16 @@ public class WaterEfect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Delta time =tiempo entre dos frames consecutivos
+        //Time.time =tiempo de juego total.
+       
         //Busca en tu profile si tienes el efecto LensDistorsion
         if(efecto.profile.TryGet(out LensDistortion distorsion))
         {
-            //distorsion.xMultiplier
+            FloatParameter xValue = new FloatParameter(1+Mathf.Cos(velocidad * Time.time)/2);
+            FloatParameter yValue = new FloatParameter(1+Mathf.Sin(velocidad * Time.time)/2);
+            distorsion.xMultiplier.SetValue(xValue);
+            distorsion.yMultiplier.SetValue(yValue);
         }
     }
 }
